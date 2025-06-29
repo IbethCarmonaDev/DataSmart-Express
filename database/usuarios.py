d6c04b7211ae1913ddb904e2f4cfff96c320946c
Ibeth Carmona <ibeth.carmona@gmail.com>
2025-06-29 09:15:42 -0500

    🛠 Junio 29 - 9

--- database/usuarios.py
+++ database/usuarios.py
@@ -17,6 +17,31 @@ def guardar_perfil_usuario(perfil):
             "dias_trial": perfil["dias_trial"]
         }
 
+        response = supabase.table("usuarios").insert([data]).execute()
+        print("🔁 Respuesta Supabase INSERT:", response)
+
+        if response.status_code != 201:
+            raise Exception(f"Error Supabase: {response.status_code} - {response.data}")
+
+        return True
+
+    except Exception as e:
+        print("❌ Error al guardar perfil:", e)
+        return False
+
+
+def OLDguardar_perfil_usuario(perfil):
+    try:
+        data = {
+            "user_id": perfil["user_id"],
+            "nombre": perfil["nombre"],
+            "email": perfil["email"],
+            "plan_actual": perfil["plan_actual"],
+            "fecha_registro": perfil["fecha_inicio_trial"],
+            "fecha_inicio_trial": perfil["fecha_inicio_trial"],
+            "dias_trial": perfil["dias_trial"]
+        }
+
         response = supabase.table("usuarios").insert([data]).execute()  # 👈 usamos listaresponse = supabase.table("usuarios").insert([data]).execute()
         print("🔁 Respuesta Supabase INSERT:", response)
 
